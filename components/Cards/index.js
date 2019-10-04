95549cfa7883cf312b2755c8292cceec407658f2
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-10-04 13:54:57 -0400

    Stretch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const card = document.querySelector('.cards-container');
+function Card(cards) {
+	const mainCard = document.createElement('div'),
+		titleCard = document.createElement('div'),
+		authorCard = document.createElement('div'),
+		imageCard = document.createElement('div'),
+		imageCardImg = document.createElement('img'),
+		nameCard = document.createElement('span');
+
+	titleCard.textContent = cards.headline;
+	imageCardImg.src = cards.authorPhoto;
+	nameCard.textContent = cards.authorName;
+
+	mainCard.classList.add('card');
+	mainCard.classList.add('headline');
+	mainCard.classList.add('author');
+	mainCard.classList.add('img-container');
+
+	mainCard.appendChild(titleCard);
+	mainCard.appendChild(authorCard);
+	mainCard.appendChild(imageCard);
+	mainCard.appendChild(imageCardImg);
+	mainCard.appendChild(nameCard);
+
+	return mainCard;
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then((response) => {
+	const articles = response.data.articles;
+	console.log(articles);
+	for (topic in articles) {
+		articles[topic].forEach((cards) => {
+			card.appendChild(Card(cards));
+		});
+	}
+});
+
+// axios.get('https://lambda-times-backend.herokuapp.com/articles').then((response) => {
+// 	console.log(response.data.card);
+// 	response.data.card.forEach((cards) => {
+// 		card.appendChild(Card(cards));
+// 	});
+// });
